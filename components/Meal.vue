3a978592c87b70eaaf9977dc15d870d5b6ea698f
Ayush <ayushbante6@gmail.com>
2021-11-29 23:02:12 +0530

    Meal App

---
+++ components/Meal.vue
@@ -0,0 +1,75 @@
+<template>
+  <div
+    class="shadow-2xl max-w-sm px-5 py-4 rounded bg-white"
+   
+  >
+    <div class="flex">
+      <div class="flex h-28 w-28 overflow-hidden rounded shadow-xl">
+        <img class="object-fit" :src="meal.strMealThumb" alt="" />
+      </div>
+      <div class="ml-2 flex-1 flex flex-col">
+          <h1 class="text-xl font-bold">{{ meal.strMeal }}</h1>
+          <div  class="my-2 w-auto rounded text-gray-500">{{ meal.strCategory }}</div>
+      </div>
+    </div>
+    <div>
+      <h1 class="text-xl font-bold mt-2">Ingredients</h1>
+      <div class="flex flex-wrap">
+        <div
+          v-for="(obj, index) in ingredient"
+          :key="index"
+          class="bg-purple-500 px-3 py-2 my-2 mx-2 rounded text-white shadow-xl"
+        >
+          {{ obj }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    meal: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      ingredients: [],
+      categories:{
+        'Beef': 'bg-yellow-700',
+        'Chicken': 'bg-red-200',
+        'Dessert': 'bg-yellow-900',
+        'Lamb': 'bg-red-200',
+        'Miscellaneous': 'bg-red-200',
+        'Pasta': 'bg-red-200',
+        'Pork': 'bg-red-200',
+        'Seafood': 'bg-green-600',
+        'Side': 'bg-red-200',
+        'Vegan': 'bg-red-200',
+        'Vegetarian': 'bg-red-200',
+        'Breakfast': 'bg-red-200',
+        'Goat': 'bg-red-500',
+      },
+      }
+    },
+  computed: {
+    ingredient: function () {
+      for (let obj in this.meal) {
+        if (obj.includes("strIngredient")) {
+          if (this.meal[obj] != null && this.meal[obj] != "") {
+            this.ingredients.push(this.meal[obj]);
+          }
+        }
+      }
+      return this.ingredients;
+    },
+  },
+
+  //  :class=" categories.hasOwnProperty(meal.strCategory) ?  categories[ meal.strCategory] : 'bg-white'"
+}
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-gray-800 flex flex-col items-center">
+    <div class="text-4xl text-center text-gray-900 my-8 bg-white px-8 py-4 rounded">
+     Random Meals
+    </div>
+    <div class="grid grid-cols-3 gap-5">
+      <Meal v-for="(meal,index) in meals" :key="index" :meal="meal" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data(){
+    return {
+      meals: [],
+      ingredients: [],
+    }
+  },
+ async  mounted() {
+
+   for (var i = 0; i <10; i++ ){
+     const recipe = (await this.$axios.get("https://www.themealdb.com/api/json/v1/1/random.php")).data
+      this.meals.push(recipe.meals[0])
+   }
+  },
+  methods:{
+    
+
+  }
+}
+</script>
